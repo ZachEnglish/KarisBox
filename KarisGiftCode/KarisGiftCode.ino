6dd647a974d4bf8aeeb7d3439e1c31f51723f474
Zach English <englishz@gmail.com>
2021-01-16 00:44:35 -0800

    Lot's of cleanup on code and one bug fix on the vibrate button logic.
    Untested in HW, but sure looks better in SW ;-)

--- KarisGiftCode/KarisGiftCode.ino
+++ KarisGiftCode/KarisGiftCode.ino
@@ -35,6 +35,16 @@
 #define RGB_WIPE_DELAY    50 //wait 50 milliseconds between changing RGB leds to the new color to give a "wipe" look
 #define RGB_RAINBOW_DELAY 1  //wait 1 millisecond between hue changes while doing the rainbow
 
+//the states for the RGB LEDs. When the button is pressed the program should cycle through these.
+enum RGB_states {
+  RGB_states_first,
+  RGB_RED_WIPE = 0,
+  RGB_GREEN_WIPE,
+  RGB_BLUE_WIPE,
+  RGB_RAINBOW,
+  RGB_states_count
+};
+
 
 //Make an object/variable to talk to the RGB strip. Tell it how many LEDs there are
 //and what pin to talk to them on. Also how to format some of the data.
@@ -47,7 +57,7 @@ unsigned long g_time_of_RGB_current_mode_start = 0;
 uint32_t      g_current_RGB_color = rgbStrip.Color(255, 0, 0);
 long          g_first_RGB_pixel_hue = 0;
 unsigned long g_last_rainbow_loop = 0;
-int           g_rgb_state = 0;
+RGB_states    g_rgb_state = RGB_states_first;
 
 
 // the setup function runs once when you power on the board or press reset
@@ -68,7 +78,6 @@ void setup() {
   rgbStrip.show(); //push out the empty buffer so the strip goes clear (in case it had something in it)
   rgbStrip.setBrightness(50); //turn down the brightness (save power and eyeballs). 0-255
 }
-
 
 
 // the loop function runs over and over again until it puts itself to sleep.
@@ -92,49 +101,90 @@ void loop() {
 
 
 void do_buttons(unsigned long current_time) {
-  static bool last_rgb_button_state = false;
-  static bool last_vibrate_button_state = false;
-  //Get the state of the buttons. Invert the logic to make true/high when pressed
-  bool current_rgb_button_state = !digitalRead(PIN_RGB_SWITCH);
-  bool current_vibrate_button_state = !digitalRead(PIN_VIBRATE_SWITCH);
-
-  //if it is pressed now and it wasn't last time we checked...
-  if (current_rgb_button_state && !last_rgb_button_state) {
+  if (rgb_button_has_been_pressed()) {
     change_rgb_mode(current_time);
     reset_sleep_timer(current_time);
   }
 
-  if (current_vibrate_button_state && !last_vibrate_button_state) {
+  if (vibrate_button_has_been_pressed()) {
     start_vibrate(current_time);
     reset_sleep_timer(current_time);
   }
-
-  last_rgb_button_state = current_rgb_button_state;
-  last_vibrate_button_state = current_rgb_button_state;
 }
 
+bool rgb_button_has_been_pressed(){
+  static bool last_rgb_button_state = false;
+  //Get the state of the button. Invert the logic to make true/high when pressed
+  bool current_rgb_button_state = !digitalRead(PIN_RGB_SWITCH);
+  bool pressed;
+
+  //if it is pressed now and it wasn't last time we checked...
+  if (current_rgb_button_state && !last_rgb_button_state) {
+    pressed = true;
+  }
+  else {
+    pressed = false;
+  }
+
+  last_rgb_button_state = current_rgb_button_state;
+  return pressed;
+}
+
+bool vibrate_button_has_been_pressed(){
+  static bool last_vibrate_button_state = false;
+  //Get the state of the button. Invert the logic to make true/high when pressed
+  bool current_vibrate_button_state = !digitalRead(PIN_VIBRATE_SWITCH);
+  bool pressed;
+
+  if (current_vibrate_button_state && !last_vibrate_button_state) {
+    pressed = true;
+  }
+  else {
+    pressed = false;
+  }
+
+  last_vibrate_button_state = current_vibrate_button_state;
+  return pressed;
+}
 
 void change_rgb_mode(unsigned long current_time) {
-  g_rgb_state++;
+  cycle_rgb_state_to_next();
   g_time_of_RGB_current_mode_start = current_time;
 
-  if (g_rgb_state > 3) {
-    g_rgb_state = 0;
-  }
   switch (g_rgb_state) {
-    case 0:
-      g_current_RGB_color = rgbStrip.Color(255, 0, 0);
+    case RGB_RED_WIPE:
+      set_current_color_to_red();
       break;
-    case 1:
-      g_current_RGB_color = rgbStrip.Color(0, 255, 0);
+    case RGB_GREEN_WIPE:
+      set_current_color_to_green();
       break;
-    case 2:
-      g_current_RGB_color = rgbStrip.Color(0, 0, 255);
+    case RGB_BLUE_WIPE:
+      set_current_color_to_blue();
       break;
-    case 3:
-      //rainbow(1); //works, know it can get here
+    case RGB_RAINBOW:
+      //Don't need to do anything here yet
       break;
   }
+}
+
+void cycle_rgb_state_to_next(){
+  g_rgb_state = g_rgb_state + 1;
+
+  if (g_rgb_state >= RGB_states_count) {
+    g_rgb_state = RGB_states_first;
+  }
+}
+
+void set_current_color_to_red(){
+  g_current_RGB_color = rgbStrip.Color(255, 0, 0);
+}
+
+void set_current_color_to_green(){
+  g_current_RGB_color = rgbStrip.Color(0, 255, 0);
+}
+
+void set_current_color_to_blue(){
+  g_current_RGB_color = rgbStrip.Color(0, 0, 255);
 }
 
 
@@ -150,28 +200,27 @@ void start_vibrate(unsigned long current_time) {
 
 
 void do_rgb(unsigned long current_time) {
-  static int last_RGB_pixel_updated = -1;
   unsigned long time_since_entering_RGB_state = current_time - g_time_of_RGB_current_mode_start;
-  int current_pixel = 0;
-  unsigned long rainbow_loop_counter = 0;
 
   switch (g_rgb_state) {
-    case 0:
-    case 1:
-    case 2:
-      current_pixel = ( time_since_entering_RGB_state / RGB_WIPE_DELAY ) % rgbStrip.numPixels();
-      if (current_pixel != last_RGB_pixel_updated) {
-        last_RGB_pixel_updated = current_pixel;
-        progress_color_wipe(current_pixel);
-      }
+    case RGB_RED_WIPE:
+    case RGB_GREEN_WIPE:
+    case RGB_BLUE_WIPE:
+      do_color_wipe(time_since_entering_RGB_state);
       break;
-    case 3:
-      rainbow_loop_counter = time_since_entering_RGB_state / RGB_RAINBOW_DELAY;
-      if (rainbow_loop_counter != g_last_rainbow_loop) {
-        g_last_rainbow_loop = rainbow_loop_counter;
-        progress_rainbow();
-      }
+    case RGB_RAINBOW:
+      do_rgb_rainbow(time_since_entering_RGB_state);
       break;
+  }
+}
+
+void do_color_wipe(unsigned long time_in_state){
+  static unsigned long last_RGB_pixel_updated = NUMBER_OF_RGB_LEDS;
+  unsigned long current_pixel = ( time_in_state / RGB_WIPE_DELAY ) % rgbStrip.numPixels();
+  
+  if (current_pixel != last_RGB_pixel_updated) {
+    last_RGB_pixel_updated = current_pixel;
+    progress_color_wipe(current_pixel);
   }
 }
 
@@ -180,15 +229,32 @@ void progress_color_wipe(int pixel) {
   rgbStrip.show();
 }
 
+void do_rgb_rainbow(unsigned long time_in_state){
+  unsigned long rainbow_loop_counter = 0;
+  
+  rainbow_loop_counter = time_in_state / RGB_RAINBOW_DELAY;
+  if (rainbow_loop_counter != g_last_rainbow_loop) {
+    g_last_rainbow_loop = rainbow_loop_counter;
+    progress_rainbow();
+  }
+}
+
 void progress_rainbow() {
+  stage_pixels_for_rainbow();
+  advance_rainbow_hue();
+  rgbStrip.show();
+}
+
+void stage_pixels_for_rainbow(){
   for (int i = 0; i < rgbStrip.numPixels(); i++) {
     int pixelHue = g_first_RGB_pixel_hue + (i * 65536L / rgbStrip.numPixels());
     rgbStrip.setPixelColor(i, rgbStrip.gamma32(rgbStrip.ColorHSV(pixelHue)));
   }
-  g_first_RGB_pixel_hue += 256;
-  rgbStrip.show();
 }
 
+void advance_rainbow_hue(){
+  g_first_RGB_pixel_hue += 256;
+}
 
 //want 0.5 second pulse, 0.5 second wait, 0.5 second pulse, 1 second wait, then repeat the pulses
 // 0.0 seconds - turned on (by start_vibrate())
@@ -203,26 +269,48 @@ void do_vibrate(unsigned long current) {
   unsigned long difference = current - g_time_of_vibration_start; //this should never go negative
 
   if (digitalRead(PIN_VIBRATE_DRIVE)) { //was on, so turn it off
-    if (
-      ( difference >= 500 && difference < 1000 ) ||
-      ( difference >= 1500 && difference < 2500 ) ||
-      ( difference >= 3000 && difference < 3500 ) ||
-      ( difference >= 4000 )
-    ) {
+    if ( should_turn_off_vibrate( difference ) ) {
       turn_off_vibrate_motor();
     }
   }
   else { //motor was off - so turn it ON
-    if (
-      ( difference >= 1000 && difference < 1500 ) ||
-      ( difference >= 2500 && difference < 3000 ) ||
-      ( difference >= 3500 && difference < 4000 )
-    ) {
+    if ( should_turn_on_vibrate( difference ) ) {
       turn_on_vibrate_motor();
     }
   }
 }
 
+// 0.5 seconds - turn off
+// 1.5 seconds - turn off
+// 3.0 seconds - turn off
+// 4.0 seconds - turn off
+bool should_turn_off_vibrate(unsigned long diff){
+  if(( diff >= 500 && diff < 1000 ) ||
+     ( diff >= 1500 && diff < 2500 ) ||
+     ( diff >= 3000 && diff < 3500 ) ||
+     ( diff >= 4000 )
+     ) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }  
+}
+
+// 1.0 seconds - turn on
+// 2.5 seconds - turn on
+// 3.5 seconds - turn on
+bool should_turn_on_vibrate(unsigned long diff){
+  if( ( diff >= 1000 && diff < 1500 ) ||
+      ( diff >= 2500 && diff < 3000 ) ||
+      ( diff >= 3500 && diff < 4000 )) {
+    return true;  
+  }
+  else {
+    return false;
+  }
+}
 
 void turn_on_vibrate_motor() {
   digitalWrite(PIN_VIBRATE_DRIVE, HIGH);
